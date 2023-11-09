e5bb156f82a7e85972b57f07589097ffe56449ff
jhs512 <jangka512@gmail.com>
2023-11-09 18:10:41 +0900

    t12, pass

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -45,23 +45,26 @@ public class App {
                 case "수정" -> {
                     final long id = rq.getParameterAsLong("id", 0);
 
-                    final Quotation quotation = quotations
+                    quotations
                             .stream()
                             .filter(_quotation -> _quotation.getId() == id)
                             .findFirst()
-                            .get();
+                            .ifPresentOrElse(
+                                    quotation -> {
+                                        System.out.println("명언(기존) : %s".formatted(quotation.getContent()));
+                                        System.out.print("명언 : ");
+                                        final String content = scanner.nextLine().trim();
+                                        System.out.println("작가(기존) : %s".formatted(quotation.getAuthorName()));
+                                        System.out.print("작가 : ");
+                                        final String authorName = scanner.nextLine().trim();
 
-                    System.out.println("명언(기존) : %s".formatted(quotation.getContent()));
-                    System.out.print("명언 : ");
-                    final String content = scanner.nextLine().trim();
-                    System.out.println("작가(기존) : %s".formatted(quotation.getAuthorName()));
-                    System.out.print("작가 : ");
-                    final String authorName = scanner.nextLine().trim();
+                                        quotation.setContent(content);
+                                        quotation.setAuthorName(authorName);
 
-                    quotation.setContent(content);
-                    quotation.setAuthorName(authorName);
-
-                    System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
+                                        System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
+                                    },
+                                    () -> System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id))
+                            );
                 }
                 case "등록" -> {
                     System.out.print("명언 : ");
