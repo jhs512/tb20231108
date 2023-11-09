047e3d84b0b6d437c1e343f0d24a711eda5d1d4f
jhs512 <jangka512@gmail.com>
2023-11-09 20:10:25 +0900

    file, t2

--- src/test/java/com/ll/standard/util/UtTest.java
+++ src/test/java/com/ll/standard/util/UtTest.java
@@ -7,12 +7,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtTest {
     @Test
-    @DisplayName("파일 생성")
+    @DisplayName("파일 생성, 삭제")
     void t1() {
-        String filePath = "temp/test.txt";
+        final String filePath = "temp/test.txt";
         Ut.file.save(filePath, "내용");
 
         assertThat(Ut.file.exists(filePath)).isTrue();
+
+        Ut.file.delete(filePath);
+    }
+
+    @Test
+    @DisplayName("파일내용 읽기")
+    void t2() {
+        final String filePath = "temp/test.txt";
+        Ut.file.save(filePath, "내용");
+
+        final String content = Ut.file.getContent(filePath);
+
+        assertThat(content).isEqualTo("내용");
 
         Ut.file.delete(filePath);
     }
