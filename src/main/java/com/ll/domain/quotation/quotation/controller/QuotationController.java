63b8d28360014b729fb60c0058ebd21e2a283fe7
jhs512 <jangka512@gmail.com>
2023-11-09 19:43:13 +0900

    mvc w11

--- src/main/java/com/ll/domain/quotation/quotation/controller/QuotationController.java
+++ src/main/java/com/ll/domain/quotation/quotation/controller/QuotationController.java
@@ -19,10 +19,9 @@ public class QuotationController {
     public void actionRemove(final Rq rq) {
         final long id = rq.getParameterAsLong("id", 0);
 
-        quotationService.findAll()
-                .stream()
-                .filter(quotation -> quotation.getId() == id)
-                .findFirst()
+        Optional<Quotation> quotationOpt = quotationService.findById(id);
+
+        quotationOpt
                 .ifPresentOrElse(
                         quotation -> {
                             quotationService.remove(quotation);
