b7dd9ece3c93063182017811431c09b59a08fd75
jhs512 <jangka512@gmail.com>
2023-11-09 18:35:44 +0900

    mvc, w6

--- src/main/java/com/ll/domain/quotation/quotation/controller/QuotationController.java
+++ src/main/java/com/ll/domain/quotation/quotation/controller/QuotationController.java
@@ -89,4 +89,13 @@ public class QuotationController {
 
         System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
     }
+
+    public void dispatch(Rq rq) {
+        switch (rq.getAction()) {
+            case "삭제" -> actionRemove(rq);
+            case "수정" -> actionModify(rq);
+            case "목록" -> actionShowList();
+            case "등록" -> actionWrite();
+        }
+    }
 }
