fe0be5f28b8a674bca7c5947b53b5e4edfc157d9
jhs512 <jangka512@gmail.com>
2023-11-09 17:16:22 +0900

    RqTest, t4, refactoring

--- src/main/java/com/ll/global/rq/Rq.java
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -1,8 +1,8 @@
 package com.ll.global.rq;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Rq {
     private final String action;
@@ -13,19 +13,13 @@ public class Rq {
         final String[] cmdBits = cmd.split("\\?", 2);
         action = cmdBits[0].trim();
         queryString = cmdBits.length == 2 ? cmdBits[1].trim() : "";
-        params = new HashMap<>();
-
-        if (queryString.isBlank()) return;
-
-        Arrays
-                .stream(queryString.split("&"))
-                .forEach(param -> {
-                    final String[] paramBits = param.split("=", 2);
-                    final String paramName = paramBits[0].trim();
-                    final String paramValue = paramBits[1].trim();
-
-                    params.put(paramName, paramValue);
-                });
+        params = Arrays.stream(queryString.split("&"))
+                .filter(param -> param.contains("="))
+                .map(param -> param.split("=", 2))
+                .collect(Collectors.toMap(
+                        paramBits -> paramBits[0].trim(),
+                        paramBits -> paramBits[1].trim(),
+                        (existing, replacement) -> replacement));
     }
 
     public String getAction() {
