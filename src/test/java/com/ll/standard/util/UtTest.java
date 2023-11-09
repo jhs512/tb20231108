5f0c067fb0e6c210453931cfe73d88dcca6ae4aa
jhs512 <jangka512@gmail.com>
2023-11-09 20:32:31 +0900

    file, t4 refactoring

--- src/test/java/com/ll/standard/util/UtTest.java
+++ src/test/java/com/ll/standard/util/UtTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtTest {
-    private final String testFilePath = "temp/test.txt";
+    private static final String TEMP_DIR = "temp/";
+    private final String testFilePath = TEMP_DIR + "test.txt";
+    private final String test2FilePath = TEMP_DIR + "test2.txt";
 
     @BeforeEach
     void beforeEach() {
@@ -18,6 +20,8 @@ public class UtTest {
     @AfterEach
     void afterEach() {
         Ut.file.delete(testFilePath);
+        // 만약 test2.txt 파일도 여러 테스트에 걸쳐 사용된다면, afterEach에서 삭제하는 것이 좋습니다.
+        Ut.file.delete(test2FilePath);
     }
 
     @Test
@@ -36,21 +40,16 @@ public class UtTest {
     @Test
     @DisplayName("파일내용을 읽은 후 long 타입으로 변환")
     void t3() {
-        final long id = Ut.file.getContentAsLong(testFilePath, 0);
-        assertThat(id).isEqualTo(0);
-
-        final String test2FilePath = "temp/test2.txt";
-        Ut.file.save(test2FilePath, 100);
+        Ut.file.save(test2FilePath, "100");
         final long age = Ut.file.getContentAsLong(test2FilePath, 0);
         assertThat(age).isEqualTo(100);
-
-        Ut.file.delete(test2FilePath);
     }
 
     @Test
     @DisplayName("없는 파일 읽으라는 시도를 하면 null 반환")
     void t4() {
-        final String content = Ut.file.getContent("temp/not-exists.txt");
+        final String nonExistentFilePath = TEMP_DIR + "not-exists.txt";
+        final String content = Ut.file.getContent(nonExistentFilePath);
 
         assertThat(content).isNull();
     }
