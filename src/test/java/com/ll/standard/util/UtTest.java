900fac967b2321cb2ab8e52a3ce011771e385740
jhs512 <jangka512@gmail.com>
2023-11-09 20:13:32 +0900

    file, t2 refactoring

--- src/test/java/com/ll/standard/util/UtTest.java
+++ src/test/java/com/ll/standard/util/UtTest.java
@@ -1,32 +1,35 @@
 package com.ll.standard.util;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtTest {
+    private final String filePath = "temp/test.txt";
+
+    @BeforeEach
+    void beforeEach() {
+        Ut.file.save(filePath, "내용");
+    }
+
+    @AfterEach
+    void afterEach() {
+        Ut.file.delete(filePath);
+    }
+
     @Test
     @DisplayName("파일 생성, 삭제")
     void t1() {
-        final String filePath = "temp/test.txt";
-        Ut.file.save(filePath, "내용");
-
         assertThat(Ut.file.exists(filePath)).isTrue();
-
-        Ut.file.delete(filePath);
     }
 
     @Test
     @DisplayName("파일내용 읽기")
     void t2() {
-        final String filePath = "temp/test.txt";
-        Ut.file.save(filePath, "내용");
-
         final String content = Ut.file.getContent(filePath);
-
         assertThat(content).isEqualTo("내용");
-
-        Ut.file.delete(filePath);
     }
 }
