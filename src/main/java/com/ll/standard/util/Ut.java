da8aa168f376cd33eeaf158771a469d8c14f718d
jhs512 <jangka512@gmail.com>
2023-11-09 20:02:39 +0900

    file, t1

---
+++ src/main/java/com/ll/standard/util/Ut.java
@@ -0,0 +1,22 @@
+package com.ll.standard.util;
+
+import lombok.SneakyThrows;
+
+public class Ut {
+    public static class file {
+
+        @SneakyThrows
+        public static void save(String filePath, String content) {
+
+        }
+
+        public static boolean exists(String filePath) {
+            return false;
+        }
+
+        @SneakyThrows
+        public static void delete(String filePath) {
+
+        }
+    }
+}
